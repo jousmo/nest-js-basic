de011e684d619197900531f2341393440a1f84b1
José Uscanga <jousmo@outlook.com>
2022-09-03 11:12:21 -0500

    feat: example routes get with params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param() params: any): object {
+    return this.appService.getProduct(params);
+  }
+
+  @Get('categories/:categoryId/products/:productId')
+  getProductsByCategory(
+    @Param('categoryId') categoryId: string,
+    @Param('productId') productId: string,
+  ): object {
+    return this.appService.getProductsByCategory(categoryId, productId);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,25 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getProduct(params: any): object {
+    const { productId } = params;
+    return {
+      id: productId,
+      name: 'Pepsi',
+      price: 15,
+    };
+  }
+
+  getProductsByCategory(categoryId: string, productId: string): object {
+    return {
+      id: categoryId,
+      name: 'Category',
+      products: {
+        id: productId,
+        name: 'Pepsi',
+        price: 15,
+      },
+    };
+  }
 }
