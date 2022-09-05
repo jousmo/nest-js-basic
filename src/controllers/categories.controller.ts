350bb3de067c1e0025c26f1cbf39d60051a45ce5
José Uscanga <jousmo@outlook.com>
2022-09-05 18:27:20 -0500

    feat: example use http status code with express and nest decorator

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,9 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Query,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { CategoriesService } from '../services/categories.service';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  getCategories(
+    @Res() response: Response,
+    @Query('limit') limit: number,
+    @Query('offset') offset: number,
+  ): any {
+    const categories = this.categoriesService.getCategories(limit, offset);
+    response.status(200).send(categories);
+  }
+
+  @Get(':categoryId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getCategory(@Param('categoryId') categoryId: string): object {
+    return this.categoriesService.getCategory(categoryId);
+  }
 
   @Get(':categoryId/products/:productId')
   getProductsByCategory(
--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -2,6 +2,30 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CategoriesService {
+  getCategories(limit = 2, offset = 0): object {
+    return {
+      limit,
+      offset,
+      categories: [
+        {
+          id: 1,
+          name: 'Category1',
+        },
+        {
+          id: 2,
+          name: 'Category2',
+        },
+      ],
+    };
+  }
+
+  getCategory(categoryId: string): object {
+    return {
+      id: categoryId,
+      name: 'Category',
+    };
+  }
+
   getProductsByCategory(categoryId: string, productId: string): object {
     return {
       id: categoryId,
