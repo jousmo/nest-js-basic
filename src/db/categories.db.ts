a0ac73cbf86dc00072f8e65d67b341738a5d8907
José Uscanga <jousmo@outlook.com>
2022-09-05 22:43:03 -0500

    feat: mock data

---
+++ src/db/categories.db.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Category1',
+  },
+  {
+    id: 2,
+    name: 'Category2',
+  },
+];
+
+export const categoriesWithProducts = [
+  {
+    id: 1,
+    name: 'Category1',
+    products: [1, 2],
+  },
+];
---
+++ src/db/products.db.ts
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Pepsi',
+    description: 'Refresco de cola',
+    price: 15,
+    stock: 100,
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 2,
+    name: 'Coca Cola',
+    description: 'Refresco de cola',
+    price: 16,
+    stock: 100,
+    image: 'https://picsum.photos/200/300',
+  },
+];
